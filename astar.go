e76d7192a547ce9a2625dbf396f4ef947f8a14b4
toukii <toukii@toukii.com>
2016-07-23 01:44:39 +0800

    next step

--- astar.go
+++ astar.go
@@ -3,97 +3,123 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 	fmt.Println("ASTAR")
 	display(data)
-	dfs(1,1)
-	//display(data)
+	dfs(1, 1)
 }
 
 var (
 	data = [][]int{
-		[]int{0,0,0,0,0,0,0,0},
-		[]int{0,0,0,0,1,0,0,0},
-		[]int{0,0,0,0,1,0,0,0},
-		[]int{0,0,0,0,1,0,0,0},
-		[]int{0,0,0,0,1,0,9,0},
-		[]int{0,0,0,0,1,0,0,0},
-		[]int{0,0,0,0,1,0,0,0},
-		[]int{0,0,0,0,1,0,0,0},
-		[]int{0,0,0,0,0,0,0,0},
+		[]int{0, 0, 0, 0, 0, 0, 0, 0},
+		[]int{0, 0, 0, 0, 1, 0, 0, 0},
+		[]int{0, 0, 0, 0, 1, 0, 0, 0},
+		[]int{0, 0, 0, 0, 1, 0, 0, 0},
+		[]int{0, 0, 0, 0, 1, 0, 9, 0},
+		[]int{0, 0, 0, 0, 1, 0, 0, 0},
+		[]int{0, 0, 0, 0, 1, 0, 0, 0},
+		[]int{0, 0, 0, 0, 1, 0, 0, 0},
+		[]int{0, 0, 0, 0, 0, 0, 0, 0},
 	}
 	visited [][]bool
-	target = 9
-	m=len(data)
-	n=len(data[0])
+	target  = 9
+	m       = len(data)
+	n       = len(data[0])
+	start   = time.Now()
 )
 
 func init() {
 	if m <= 0 {
 		return
 	}
-	visited = make([][]bool,m)
+	visited = make([][]bool, m)
 	for i := 0; i < m; i++ {
-		visited[i]=make([]bool,n)
+		visited[i] = make([]bool, n)
 	}
 }
 
-func display(data [][]int)  {
-	for _,it:= range data{
-		for _,itt:=range it{
-			fmt.Print(itt,"  ")
+/**
+display map
+ */
+func display(data [][]int) {
+	for _, it := range data {
+		for _, itt := range it {
+			fmt.Print(itt, "  ")
 		}
 		fmt.Println()
 	}
 }
 
-func rute(r [][]bool)  {
-	for _,it:= range r{
-		for _,itt:=range it{
-			fmt.Print(itt,"  ")
+/**
+print the current rute
+ */
+func rute(r [][]bool) {
+	for _, it := range r {
+		for _, itt := range it {
+			fmt.Print(itt, "  ")
 		}
 		fmt.Println()
 	}
 }
 
-func dfs(i,j int)  {
+/**
+dfs algorithm
+ */
+func dfs(i, j int) {
 	v := data[i][j]
-	if v==target {
-		fmt.Println("target is found.")
+	fmt.Println(fmt.Sprintf("(%d,%d):%d", i, j, v))
+	display(data)
+	if v == target {
+		fmt.Println("target is found, costs ", time.Since(start))
 		os.Exit(0)
 	}
-	if visited[i][j] || v!=0 {
+	if v != 0 {
 		return
 	}
-	fmt.Println(fmt.Sprintf("(%d,%d):%d",i,j,v))
-	display(data)
-	visited[i][j]=true
-	data[i][j]=2
-	dfs(next(i,j))
-	visited[i][j]=false
-	data[i][j]=0
+	visited[i][j] = true
+	data[i][j] = 2
+	for _, it := range next(i, j) {
+		dfs(it.i, it.j)
+	}
+	visited[i][j] = false
+	data[i][j] = 0
 }
 
-func next(i, j int) (int,int) {
-	if safe(i-1,j) {
-		return i-1,j
-	}
-	if safe(i+1,j) {
-		return i+1,j
-	}
-	if safe(i, j - 1) {
-		return i,j-1
-	}
-	if safe(i, j + 1) {
-		return i,j+1
-	}
-	return i,j
+/**
+visit step
+ */
+type vst struct {
+	i, j int
 }
 
-func safe(i,j int)bool  {
-	if i>=m||j>=n|| j<0||i<0 {
+/**
+return all the posibile rute of (i,j): up,down,left and right
+ */
+func next(i, j int) []vst {
+	vsts := make([]vst, 0, 4)
+	if safe(i-1, j) {
+		vsts = append(vsts, vst{i: i - 1, j: j})
+	}
+	if safe(i+1, j) {
+		vsts = append(vsts, vst{i: i + 1, j: j})
+	}
+	if safe(i, j-1) {
+		vsts = append(vsts, vst{i: i, j: j - 1})
+	}
+	if safe(i, j+1) {
+		vsts = append(vsts, vst{i: i, j: j + 1})
+	}
+	return vsts
+}
+
+/**
+check the next step if safe: out of range or visited
+ */
+func safe(i, j int) bool {
+	if i >= m || j >= n || j < 0 || i < 0 {
 		return false
 	}
 	if !visited[i][j] {
