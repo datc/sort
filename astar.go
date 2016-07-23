f91a9388e9f8cbf89f7f5d9f4190842d315cc26d
toukii <toukii@toukii.com>
2016-07-23 09:09:43 +0800

    log

--- astar.go
+++ astar.go
@@ -2,33 +2,39 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"sort"
 	"time"
 )
 
 func main() {
 	fmt.Println("ASTAR\t\t")
 	display(data)
-	dfs(1, 1)
+	dfs(startVst.i, startVst.j)
 }
 
 var (
 	data = [][]int{
 		[]int{0, 0, 0, 0, 0, 0, 0, 0},
+		[]int{0, 7, 0, 0, 1, 0, 0, 0},
 		[]int{0, 0, 0, 0, 1, 0, 0, 0},
 		[]int{0, 0, 0, 0, 1, 0, 0, 0},
 		[]int{0, 0, 0, 0, 1, 0, 0, 0},
-		[]int{0, 0, 0, 0, 1, 0, 9, 0},
 		[]int{0, 0, 0, 0, 1, 0, 0, 0},
 		[]int{0, 0, 0, 0, 1, 0, 0, 0},
 		[]int{0, 0, 0, 0, 1, 0, 0, 0},
 		[]int{0, 0, 0, 0, 0, 0, 0, 0},
 	}
-	visited [][]bool
-	target  = 9
-	m       = len(data)
-	n       = len(data[0])
-	start   = time.Now()
+	visited   [][]bool
+	startV    = 7
+	targetV   = 9
+	startVst  = vst{i: 1, j: 1}
+	obstacle  = 1
+	targetVst = vst{i: 6, j: 6}
+	m         = len(data)
+	n         = len(data[0])
+	start     = time.Now()
 )
 
 func init() {
@@ -39,11 +45,12 @@ func init() {
 	for i := 0; i < m; i++ {
 		visited[i] = make([]bool, n)
 	}
+	data[targetVst.i][targetVst.j] = targetV
 }
 
 /**
 display map
- */
+*/
 func display(data [][]int) {
 	for _, it := range data {
 		for _, itt := range it {
@@ -55,7 +62,7 @@ func display(data [][]int) {
 
 /**
 print the current rute
- */
+*/
 func rute(r [][]bool) {
 	for _, it := range r {
 		for _, itt := range it {
@@ -67,20 +74,18 @@ func rute(r [][]bool) {
 
 /**
 dfs algorithm
- */
+*/
 func dfs(i, j int) {
 	v := data[i][j]
-	fmt.Println(fmt.Sprintf("(%d,%d):%d\t\t", i, j, v))
-	display(data)
-	if v == target {
-		fmt.Println("target is found, costs ", time.Since(start))
-		os.Exit(0)
-	}
-	if v != 0 {
+	if v == obstacle {
 		return
 	}
+	fmt.Println(fmt.Sprintf("(%d,%d):%d\t\t", i, j, v))
+	if !startVst.reached(i, j) {
+		data[i][j] = 2
+	}
+	display(data)
 	visited[i][j] = true
-	data[i][j] = 2
 	for _, it := range next(i, j) {
 		dfs(it.i, it.j)
 	}
@@ -90,14 +95,46 @@ func dfs(i, j int) {
 
 /**
 visit step
- */
+*/
 type vst struct {
 	i, j int
 }
 
 /**
+reached the some position
+*/
+func (v vst) reached(i, j int) bool {
+	return i == v.i && j == v.j
+}
+
+/**
+distance to the target position
+*/
+func (v vst) dis(trgt vst) int {
+	return (int(math.Abs(float64(trgt.i-v.i)) + math.Abs(float64(trgt.j-v.j))))
+}
+
+type vstSlc []vst
+
+func (v vstSlc) Len() int {
+	return len(v)
+}
+
+func (v vstSlc) Less(i, j int) bool {
+	return v[i].dis(targetVst) < v[j].dis(targetVst)
+}
+
+func (v vstSlc) Swap(i, j int) {
+	v[i], v[j] = v[j], v[i]
+}
+
+func (v vstSlc) Sort() {
+	sort.Sort(v)
+}
+
+/**
 return all the posibile rute of (i,j): up,down,left and right
- */
+*/
 func next(i, j int) []vst {
 	vsts := make([]vst, 0, 4)
 	if safe(i-1, j) {
@@ -112,15 +149,22 @@ func next(i, j int) []vst {
 	if safe(i, j+1) {
 		vsts = append(vsts, vst{i: i, j: j + 1})
 	}
+	vs := vstSlc(vsts)
+	vs.Sort()
+	_ = vs
 	return vsts
 }
 
 /**
 check the next step if safe: out of range or visited
- */
+*/
 func safe(i, j int) bool {
 	if i >= m || j >= n || j < 0 || i < 0 {
 		return false
+	}
+	if targetVst.reached(i, j) {
+		fmt.Println("target is found, costs ", time.Since(start))
+		os.Exit(0)
 	}
 	if !visited[i][j] {
 		return true
